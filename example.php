3173bfc4a9e065a4dcff26a5dbca4ae9b910d4a7
NatcharinJanhom <36236798+NatcharinJanhom@users.noreply.github.com>
2018-08-28 22:50:28 +0700

    new

---
+++ example.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include('head.php'); ?>    
+  </head>
+  <body class="sidebar-icon-only">
+    <div class="container-scroller">
+    <?php include('navbar.php'); ?>   
+      <div class="container-fluid page-body-wrapper">        
+      <?php include('sidebar.php'); ?>    
+        <div class="main-panel">
+            <div class="content-wrapper">
+            <!-- หน้าเว็บ -->
+            </div>
+         
+          
+            <?php include('footer.php'); ?> 
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    </body>
+</html>
---
+++ head.php
@@ -0,0 +1,35 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Star Admin Premium Bootstrap Admin Dashboard Template</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="theme/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">-->
+    <link rel="stylesheet" href="theme/assets/vendors/iconfonts/puse-icon-feather/feather.css">
+    <link rel="stylesheet" href="theme/assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="theme/assets/vendors/css/vendor.bundle.addons.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="theme/assets/vendors/iconfonts/flag-icon-css/flag-icon.min.css" />
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="theme/assets/css/shared/style.css">
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="theme/assets/css/demo_1/style.css">
+    <!-- End Layout styles -->
+    <!-- plugins:js -->
+    <script src="theme/assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="theme/assets/vendors/js/vendor.bundle.addons.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="theme/assets/js/shared/off-canvas.js"></script>
+    <script src="theme/assets/js/shared/hoverable-collapse.js"></script>
+    <script src="theme/assets/js/shared/misc.js"></script>
+    <script src="theme/assets/js/shared/settings.js"></script>
+    <script src="theme/assets/js/shared/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="theme/assets/js/demo_1/dashboard.js"></script>
+    <!-- End custom js for this page-->
