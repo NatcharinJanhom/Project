40eb5ec051b418fbe3f87a633e05e6440e856648
panuwat39 <panuwat39t@gmail.com>
2018-08-30 13:49:46 +0700

    few

---
+++ projecttest.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <div class="container">
+        <div>
+            <h1>Admin<button type = button class="fa fa-plus " onclick="window.location='p1.php'"> </button></h1><BR>     
+            <div class = 'card'>
+                <div class="card-body">
+                <button type="button" class="fa fa-cog btn btn-light" onclick="window.location='p1.php'"></button>
+                <button type="button" class="fa fa-trash btn btn-light"></button>
+                <button type="button" class="fa fa-eye-slash btn btn-light"></button>
+                <button type="button" class="fa fa-plus btn btn-link  ">เพิ่มข้อมูล</button>
+                
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
